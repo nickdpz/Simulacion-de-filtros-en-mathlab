8adf2142804a4fa3bdb400d487c84e7ebd16f4cd
nickdpz <ndpastranz@correo.udistrital.edu.co>
2018-02-11 00:30:27 -0500

    Se añaden todos los filtros comprovados en matlab por bode

---
+++ StepSegundoOrden2.m
@@ -0,0 +1,9 @@
+fig=figure('Name','Autor Nicolas David Pastran','NumberTitle','off','Color','white','position',[250 200 850 400]);       
+cla('reset')
+zi=0.7;
+wn1=3000;
+dem=[1 2*zi*wn1 wn1^2];
+num=wn1^2;
+h=tf(num,dem);
+step(h);
+grid on;
