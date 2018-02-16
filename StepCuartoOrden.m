dfad9e75072471714ee0bb3f9235685867c82898
DavidSebastian119 <sebasjimenez002@hotmail.com>
2018-02-15 19:56:49 -0500

    Se añaden todos los archivos practico

--- StepCuartoOrden.m
+++ StepCuartoOrden.m
@@ -3,13 +3,19 @@ cla('reset')
 datos = csvread("CuartoOrden.dat",1,0);
 t=(datos(:,1))';
 v=(datos(:,2))';
-plot(t,v,'--b');hold on;
+datose = csvread("CuartoOrdenp.csv",2,0);
+te=(datose(:,1)');
+ve=(datose(:,2)'/2);
+plot(t,v,'Square','Color','red');
+hold on;
+plot(te,ve,'-','Color','black');
+hold on;
 dem=[1 5200 14.2e6 13.2e9 9e12];
 num=9e12;
 h=tf(num,dem);
-step(h,'r');
+step(h,'--blue');
 grid on;
-legend('Simulacion Proteus','Simulacion MatLab')
+legend('Simulacion Proteus','Datos Reales','Simulacion MatLab')
 legend('Location','northeastoutside')
 legend('boxoff')
 grid on;
--- StepTercerOrden.m
+++ StepTercerOrden.m
@@ -3,13 +3,19 @@ cla('reset')
 datos = csvread("TercerOrden.dat",1,0);
 t=(datos(:,1))';
 v=(datos(:,2))';
-plot(t,v,'--b');hold on;
+datose = csvread("TercerOrdenp.csv",2,0);
+te=(datose(:,1)');
+ve=(datose(:,2)'/2);
+plot(t,v,'Square','Color','red');
+hold on;
+plot(te,ve,'-','Color','black');
+hold on;
 dem=[1 3000 3e6 2e9];
 num=2e9;
 h=tf(num,dem);
-step(h,'r');
+step(h,'--blue');
 grid on;
-legend('Simulacion Proteus','Simulacion MatLab')
+legend('Simulacion Proteus','Datos Reales','Simulacion MatLab')
 legend('Location','northeastoutside')
 legend('boxoff')
 grid on;
